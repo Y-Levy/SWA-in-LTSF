632aa0e9706ab48d38336f1253b3894bcfc11958
Y-Levy <63908772+Y-Levy@users.noreply.github.com>
2024-05-26 11:53:21 +0300

    Update and rename PatchTST_layers.py to patchTST_layers.py

--- layers/patchTST_layers.py
+++ layers/patchTST_layers.py
@@ -10,7 +10,6 @@ class Transpose(nn.Module):
         self.dims, self.contiguous = dims, contiguous
     def forward(self, x):
         if self.contiguous:
-            assert x.transpose(*self.dims).contiguous().device() == 'cuda:0', 'PatchTST_layers.py -> Transpose -> forward -> if'
             return x.transpose(*self.dims).contiguous()
         else:
             return x.transpose(*self.dims)
@@ -41,7 +40,6 @@ class moving_avg(nn.Module):
         x = torch.cat([front, x, end], dim=1)
         x = self.avg(x.permute(0, 2, 1))
         x = x.permute(0, 2, 1)
-        assert x.device == 'cuda:0', 'PatchTST_layers.py -> moving_avg -> forward'
         return x
 
 
@@ -73,7 +71,6 @@ def PositionalEncoding(q_len, d_model, normalize=True):
     if normalize:
         pe = pe - pe.mean()
         pe = pe / (pe.std() * 10)
-    assert pe.device == 'cuda:0', 'PatchTST_layers.py -> PositionalEncoding'
     return pe
 
 SinCosPosEncoding = PositionalEncoding
@@ -91,7 +88,6 @@ def Coord2dPosEncoding(q_len, d_model, exponential=False, normalize=True, eps=1e
     if normalize:
         cpe = cpe - cpe.mean()
         cpe = cpe / (cpe.std() * 10)
-    assert cpe.device == 'cuda:0', 'PatchTST_layers.py -> Coord2dPosEncoding'
     return cpe
 
 def Coord1dPosEncoding(q_len, exponential=False, normalize=True):
@@ -99,7 +95,6 @@ def Coord1dPosEncoding(q_len, exponential=False, normalize=True):
     if normalize:
         cpe = cpe - cpe.mean()
         cpe = cpe / (cpe.std() * 10)
-    assert cpe.device == 'cuda:0', 'PatchTST_layers.py -> Coord1dPosEncoding'
     return cpe
 
 def positional_encoding(pe, learn_pe, q_len, d_model):
@@ -108,39 +103,28 @@ def positional_encoding(pe, learn_pe, q_len, d_model):
         W_pos = torch.empty((q_len, d_model)) # pe = None and learn_pe = False can be used to measure impact of pe
         nn.init.uniform_(W_pos, -0.02, 0.02)
         learn_pe = False
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> None'
     elif pe == 'zero':
         W_pos = torch.empty((q_len, 1))
         nn.init.uniform_(W_pos, -0.02, 0.02)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> zero'
     elif pe == 'zeros':
         W_pos = torch.empty((q_len, d_model)).to(torch.device('cuda:{}'.format(0)))
         nn.init.uniform_(W_pos, -0.02, 0.02).to(torch.device('cuda:{}'.format(0)))
-        assert W_pos.device.type == 'cuda', 'PatchTST_layers.py -> positional_encoding -> zeros'
-        assert W_pos.device.index == 0, 'PatchTST_layers.py -> positional_encoding -> zeros2'
     elif pe == 'normal' or pe == 'gauss':
         W_pos = torch.zeros((q_len, 1))
         torch.nn.init.normal_(W_pos, mean=0.0, std=0.1)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> normal'
     elif pe == 'uniform':
         W_pos = torch.zeros((q_len, 1))
         nn.init.uniform_(W_pos, a=0.0, b=0.1)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> uniform'
     elif pe == 'lin1d':
         W_pos = Coord1dPosEncoding(q_len, exponential=False, normalize=True)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> lin1d'
     elif pe == 'exp1d':
         W_pos = Coord1dPosEncoding(q_len, exponential=True, normalize=True)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> exp1d'
     elif pe == 'lin2d':
         W_pos = Coord2dPosEncoding(q_len, d_model, exponential=False, normalize=True)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> lin2d'
     elif pe == 'exp2d':
         W_pos = Coord2dPosEncoding(q_len, d_model, exponential=True, normalize=True)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> exp2d'
     elif pe == 'sincos':
         W_pos = PositionalEncoding(q_len, d_model, normalize=True)
-        assert W_pos.device == 'cuda:0', 'PatchTST_layers.py -> positional_encoding -> sincos'
     else: raise ValueError(f"{pe} is not a valid pe (positional encoder. Available types: 'gauss'=='normal', \
         'zeros', 'zero', uniform', 'lin1d', 'exp1d', 'lin2d', 'exp2d', 'sincos', None.)")
     return nn.Parameter(W_pos, requires_grad=learn_pe)
