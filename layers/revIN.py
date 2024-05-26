db216a06994aa7711286a9bad2576f853aa99cef
Y-Levy <63908772+Y-Levy@users.noreply.github.com>
2024-05-26 11:55:15 +0300

    Update and rename RevIN.py to revIN.py

--- layers/revIN.py
+++ layers/revIN.py
@@ -25,26 +25,19 @@ class RevIN(nn.Module):
         elif mode == 'denorm':
             x = self._denormalize(x)
         else: raise NotImplementedError
-        x.to(torch.device('cuda:{}'.format(0)))
-        assert x.device.type == 'cuda', 'RevIN.py -> RevIN -> forward -> x'
         return x
 
     def _init_params(self):
         # initialize RevIN params: (C,)
         self.affine_weight = nn.Parameter(torch.ones(self.num_features))
-        assert self.affine_weight.device == 'cuda:0', 'RevIN.py -> RevIN -> _init_params -> self.affine_weight'
         self.affine_bias = nn.Parameter(torch.zeros(self.num_features))
-        assert self.affine_bias.device == 'cuda:0', 'RevIN.py -> RevIN -> _init_params -> self.affine_bias'
 
     def _get_statistics(self, x):
         dim2reduce = tuple(range(1, x.ndim-1))
         if self.subtract_last:
             self.last = x[:,-1,:].unsqueeze(1)
-            assert self.last.device == 'cuda:0', 'RevIN.py -> RevIN -> _get_statistics -> self.last'
         else:
             self.mean = torch.mean(x, dim=dim2reduce, keepdim=True).detach().to(torch.device('cuda:{}'.format(0)))
-            assert self.mean.device.type == 'cuda', 'RevIN.py -> RevIN -> _get_statistics -> self.mean'
-            assert self.mean.device.index == 0, 'RevIN.py -> RevIN -> _get_statistics -> self.mean2'
         self.stdev = torch.sqrt(torch.var(x, dim=dim2reduce, keepdim=True, unbiased=False) + self.eps).detach().to(torch.device('cuda:{}'.format(0)))
         assert self.stdev.device.type == 'cuda', 'RevIN.py -> RevIN -> _get_statistics -> self.stdev'
 
@@ -57,8 +50,6 @@ class RevIN(nn.Module):
         if self.affine:
             x = x * self.affine_weight
             x = x + self.affine_bias
-        x.to(torch.device('cuda:{}'.format(0)))
-        assert x.device.type == 'cuda', 'RevIN.py -> RevIN -> _normalize -> x'
         return x
 
     def _denormalize(self, x):
@@ -70,5 +61,4 @@ class RevIN(nn.Module):
             x = x + self.last
         else:
             x = x + self.mean
-        assert x.device.type == 'cuda', 'RevIN.py -> RevIN -> _denormalize -> x'
         return x
